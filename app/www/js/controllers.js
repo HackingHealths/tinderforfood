05d383d58b6bfb3652e1c01dd852cec1c87f2677
Eugene Choi <eugene.maestro.choi@gmail.com>
2014-09-26 22:36:44 +0800

    added swipe left and swipe right

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,6 +1,24 @@
+'use strict';
+
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope, $ionicPopup) {
+  $scope.swipeLeft = function () { 
+    console.log('swiped left');
+    // var alertPopup = 
+    $ionicPopup.alert({
+     title: 'Swiped left!',
+     // template: 'It might taste good'
+    });
+  };
+  $scope.swipeRight = function () {
+    console.log('swiped right');
+
+    $ionicPopup.alert({
+      title: 'Swiped right!',
+     // template: 'It might taste good'
+    });
+  };
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
