21ac8ed79a004226283a3b208a7627c8c73f49f5
Harry Chen <harrychenca@gmail.com>
2014-09-28 12:45:13 +0800

    Show total number of correct answers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -295,6 +295,7 @@ angular.module('starter.controllers', [])
     // }
     //result of the 10 questions that user just answered
     var userResults = results;
+    var numTotalRight = 0;
 
     resultRef.once('value', function (snapshot) {
       var otherResults = snapshot.val();
@@ -313,8 +314,13 @@ angular.module('starter.controllers', [])
         //   category: category,
         //   result: result
         // }
+        if (userResults[i].result){
+          numTotalRight++;
+        }
       }
     });
+    console.log(numTotalRight);
+    $scope.numTotalRight = numTotalRight;
     $scope.results = userResults;
     console.log($scope.results);
   });
